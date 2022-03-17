797ee50074cf136bbb9c42138cfa5d3bf023d825
Eric <896853205@qq.com>
2022-03-17 11:52:13 +0800

    feat: change About describe

--- src/content/About.tsx
+++ src/content/About.tsx
@@ -5,10 +5,9 @@ function About() {
         <img className="w-4/12 mr-10 rounded-2xl shadow-md" src="/images/about/me.jpeg" alt="" />
         <div className="text-xl">
           <h2 className="text-2xl">About Me</h2>
-          <p>Hi there!</p>
-          <p>I’m a Visual Development Artist and Background Painter. I’m currently studying Illustration Entertainment Arts at ArtCenter College of Design in Pasadena, CA. Previously, I studied Graphic Design at Cal Poly Pomona.</p>
-          <p>Drawing has been the love of my life since I can remember, and diving into the world of animation has only strengthened my bond! With everything I paint, I make sure to integrate my passion for storytelling along with my love for design.</p>
-          <p>When I’m not drawing, you can find me cuddling my dog Sugar or devouring boba by the liters.</p>
+          <p>Hello!</p>
+          <p>My name is Luna Jiang, an illustrator and designer who wants to work for video games. This goal is the driving force of my creation. My work is influenced by a lot of games, with strong colors and rich details.</p>
+          <p>I love telling stories in my works, and exploring the relationship between characters and backgrounds keeps me entertained.</p>
           <button className="bg-black border-none text-white flex items-center justify-center pt-3 cursor-pointer px-3">RESUME</button>
         </div>
       </div>
